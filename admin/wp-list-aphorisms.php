b20bbd9cba641e8ff3d3a8a82ba58ddd87e18333
Annala <n1anje00@students.oamk.fi>
2022-10-07 11:53:03 +0300

    Getting aphorisms from the database

--- admin/wp-list-aphorisms.php
+++ admin/wp-list-aphorisms.php
@@ -5,9 +5,24 @@
     global $wpdb;
 
     //Table name for SQL statement
-    
+    $table_name = "classic-aphorism";
 
+    //Run SQL statement for querying the number of aphorism in database
+    $aphorism_count = $wpdb->get_var("SELECT COUNT(id) FROM" . $table_name):
+    echo "<h2>Aphorisms (" . $aphorism_count . ")</h2>";
+    if ($aphorism_count > 0) {
 
+        //SQL statement for getting all aphorisms from database
+        $sql = "SELECT * FROM " . $table_name;
 
+        //Run SQL statement
+        $aphorisms = $wpdb->get_results($sql);
 
+        //If there are aphorims, iterate those and output to screen
+        if ($aphorisms) {
+            foreach ($aphprisms as $aphorism) {
+                echo "<p>" . $aphorism-> aphorism . "</p>";
+            }
+        }
+    }
 ?>
